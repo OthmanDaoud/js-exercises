d60768f1b673df6a67e7e65553c683be63d4720c
unknown <o.daoued@gmail.com>
2024-06-04 17:06:22 +0300

    JS Exerciese

---
+++ app.js
@@ -0,0 +1,39 @@
+// Q7
+// for (let i = 0; i < 6; ++i) {
+//   alert(i);
+// }
+
+// Q8
+// nums = [];
+// for (let i = 0; i < 6; ++i) {
+//   nums.push(i);
+// }
+// alert(nums);
+
+// Q9
+// nums = [];
+// for (let i = 0; i < 21; ++i) {
+//   if (i % 3 == 0) {
+//     nums.push(i);
+//   }
+// }
+
+// alert(nums);
+
+// Q10
+// num = prompt("Enter a number in the range 0-100");
+// if (num < 101 && num > -1) {
+//   alert(num);
+// } else {
+//   alert("Error try again");
+// }
+
+// Q11;
+// num = Number(prompt("Enter a Number"));
+// let sum = 0;
+// for (let i = 0; i <= num; i++) {
+//   sum += i;
+//   if (i == num) {
+//     alert(sum);
+//   }
+// }
